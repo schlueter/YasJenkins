496a5f4689ae31b4a401fb7a760ccf3214ee9a78
Schlueter <b@schlueter.blue>
2019-10-24 23:05:48 -0700

    bump alpha version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='YasJenkinsHandler',
     description='A Jenkins handler for YAS',
-    version='1.0-alpha1',
+    version='1.0-alpha2',
     packages=find_packages(),
     install_requires=[
         'yas',
