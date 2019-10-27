b4f016bbdcec71791beb5af03e05ffef353ce0f2
Schlueter <b@schlueter.blue>
2019-10-26 17:48:17 -0700

    Bump version to rc1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A Jenkins handler for YAS',
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    version='1.0-beta2',
+    version='1.0-rc1',
     packages=find_packages(),
     install_requires=[
         'yas',
